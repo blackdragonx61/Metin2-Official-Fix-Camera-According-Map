49fd0aa1cace0e2aa493a3cac85caf6237fc8c88
blackdragonx61 <118901736+blackdragonx61@users.noreply.github.com>
2023-01-10 20:00:10 +0300

    Add files via upload

---
+++ 1.Svn/Client/UserInterface/PythonApplicationModule.cpp
@@ -0,0 +1,31 @@
+//Find
+PyObject* appSetCameraMaxDistance(PyObject* poSelf, PyObject* poArgs)
+{
+	...
+}
+
+///Change
+PyObject* appSetCameraMaxDistance(PyObject* poSelf, PyObject* poArgs)
+{
+	float fMax;
+	if (!PyTuple_GetFloat(poArgs, 0, &fMax))
+		return Py_BuildException();
+
+	const std::string c_rstrMapFileName = CPythonBackground::Instance().GetWarpMapName();
+
+	if (!c_rstrMapFileName.compare("metin2_map_n_flame_dragon"))
+		fMax = 6000.0f;
+	else if (!c_rstrMapFileName.compare("metin2_12zi_stage"))
+		fMax = 5000.0f;
+	else if (!c_rstrMapFileName.compare("metin2_map_defensewave"))
+		fMax = 5000.0f;
+	else if (!c_rstrMapFileName.compare("metin2_map_miniboss_01"))
+		fMax = 5000.0f;
+	else if (!c_rstrMapFileName.compare("metin2_map_miniboss_02"))
+		fMax = 5000.0f;
+	else if (!c_rstrMapFileName.compare("metin2_map_mists_of_island"))
+		fMax = 5000.0f;
+
+	CCamera::SetCameraMaxDistance(fMax);
+	return Py_BuildNone();
+}
