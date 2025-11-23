69b8efcdde6cd546b4ebcb9c9d0c7aa178075ca4
blackdragonx61 <118901736+blackdragonx61@users.noreply.github.com>
2025-11-23 11:50:25 +0300

    Update PythonApplicationModule.cpp

--- 1.Svn/Client/UserInterface/PythonApplicationModule.cpp
+++ 1.Svn/Client/UserInterface/PythonApplicationModule.cpp
@@ -7,30 +7,32 @@ PyObject* appSetCameraMaxDistance(PyObject* poSelf, PyObject* poArgs)
 ///Change
 PyObject* appSetCameraMaxDistance(PyObject* poSelf, PyObject* poArgs)
 {
-	float fMax;
-	if (!PyTuple_GetFloat(poArgs, 0, &fMax))
-		return Py_BuildException();
+	float fMax = 0.0f;
+	
+	static const std::map<std::string, std::pair<float, float>> CameraDistanceMap = {
+		{ "metin2_map_n_flame_dragon", std::make_pair(6000.0f, 6000.0f) },
+		{ "metin2_12zi_stage", std::make_pair(5000.0f, 5000.0f) },
+		{ "metin2_map_defensewave", std::make_pair(5000.0f, 5000.0f) },
+		{ "metin2_map_miniboss_01", std::make_pair(5000.0f, 5000.0f) },
+		{ "metin2_map_miniboss_02", std::make_pair(5000.0f, 5000.0f) },
+		{ "metin2_map_mists_of_island", std::make_pair(5000.0f, 5000.0f) },
+		{ "metin2_map_guild_whitedragon_boss", std::make_pair(7000.0f, 7000.0f) },
+		{ "metin2_map_moonlight_boss", std::make_pair(2500.0f, 5500.0f) },
+		{ "metin2_map_greedy_room", std::make_pair(7000.0f, 7000.0f) },
+	};
 
 	const std::string c_rstrMapFileName = CPythonBackground::Instance().GetWarpMapName();
-
-	if (!c_rstrMapFileName.compare("metin2_map_n_flame_dragon"))
-		fMax = 6000.0f;
-	else if (!c_rstrMapFileName.compare("metin2_12zi_stage"))
-		fMax = 5000.0f;
-	else if (!c_rstrMapFileName.compare("metin2_map_defensewave"))
-		fMax = 5000.0f;
-	else if (!c_rstrMapFileName.compare("metin2_map_miniboss_01"))
-		fMax = 5000.0f;
-	else if (!c_rstrMapFileName.compare("metin2_map_miniboss_02"))
-		fMax = 5000.0f;
-	else if (!c_rstrMapFileName.compare("metin2_map_mists_of_island"))
-		fMax = 5000.0f;
-	else if (!c_rstrMapFileName.compare("metin2_map_guild_whitedragon_boss"))
-		fMax = 7000.0f;
-	else if (!c_rstrMapFileName.compare("metin2_map_moonlight_boss"))
-		fMax = 5500.0f;
-	else if (!c_rstrMapFileName.compare("metin2_map_greedy_room"))
-		fMax = 7000.0f;
+	auto it = CameraDistanceMap.find(c_rstrMapFileName);
+	if (it == CameraDistanceMap.end())
+	{
+		if (!PyTuple_GetFloat(poArgs, 0, &fMax))
+			return Py_BuildException();
+	}
+	else
+	{
+		const std::pair<float, float>& distance = it->second;
+		fMax = CPythonSystem::Instance().GetCameraMode() ? distance.second : distance.first;
+	}
 
 	CCamera::SetCameraMaxDistance(fMax);
 	return Py_BuildNone();
